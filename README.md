30bce0794677d702b47b71901703a798b3766591
slime-man <slime-man@users.noreply.github.com>
2018-10-04 20:35:32 -0400

    Add KeyEvent Example (#1)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Ruby 2D Examples
+
+## KeyEvent
+An example using the `on` method to capture `KeyEvents`.
+It displays the keys you type, like stamps on the screen. 
+
+Press **space** to clear, and **backspace** to quit.
---
+++ keyevent-example.rb
@@ -0,0 +1,32 @@
+#
+# Ruby2D Example: Key Events
+#
+# This is a simple example capturing KeyEvents. Run it and try typing!
+# => author: slime-man
+require 'ruby2d'
+
+set title: "KeyEvent Example"
+text_size = 20
+font_path = "fonts/Ubuntu.ttf"
+
+on :key_down do |k|
+  key = k['key']
+  case key
+  when 'space'
+    clear
+  when 'backspace'
+    close
+  else
+    Text.new(
+      x: rand((get :width) - text_size),
+      y: rand((get :height) - text_size),
+      text: key,
+      size: text_size,
+      font: font_path,
+      z: 0,
+      color: 'random'
+    )
+  end
+end
+
+show
